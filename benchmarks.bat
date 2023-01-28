7e5b3d793b15a1020c11206cc76002bc4a363b9b
renzhong <lu631992923q@gmail.com>
2023-01-28 23:27:37 +0100

    add bat file of experiment

--- benchmarks.bat
+++ benchmarks.bat
@@ -1,6 +1,43 @@
+@echo off
+SETLOCAL
+CALL :Start KGs/UMLS , Pykeen_DistMult
+CALL :Start KGs/UMLS , Pykeen_ComplEx
+@REM CALL :Start KGs/UMLS , DistMult
+@REM CALL :Start KGs/UMLS , ConEx
+@REM CALL :Start KGs/KINSHIP , Pykeen_DistMult
+@REM CALL :Start KGs/KINSHIP , Pykeen_ComplEx
+@REM CALL :Start KGs/KINSHIP , DistMult
+@REM CALL :Start KGs/KINSHIP , DistMult
+EXIT /B %ERRORLEVEL%
 
-echo run benchmarks...
-
-python main.py --path_dataset_folder "KGs/UMLS" --model "Pykeen_DistMult" --num_epochs 10 --scoring_technique "NegSample" ^
---batch_size 1024 --lr 0.1 --embedding_dim 64 --trainer "PL" --neg_ratio 1 --pykeen_model_kwargs embedding_dim=64 loss="bcewithlogits" ^
+:Start
+python main.py --path_dataset_folder %~1 --model %~2 --num_epochs 250 --scoring_technique "NegSample" ^
+--batch_size 1024 --lr 0.1 --embedding_dim 256 --trainer "PL" --neg_ratio 1 --pykeen_model_kwargs embedding_dim=64 loss="CrossEntropy" ^
 --use_SLCWALitModule --save_embeddings_as_csv --eval_model "train_val_test" --num_core 1 --accelerator "gpu" --devices 1
+
+EXIT /B 0
+
+
+@REM echo run benchmarks...
+@REM SET path1=KGs/UMLS
+@REM SET path2=KGs/KINSHIP
+@REM SET pykeenModel1=Pykeen_DistMult
+@REM SET pykeenModel2=Pykeen_ComplEx
+@REM SET diceModel1=DistMult
+@REM SET diceModel2=DistMult
+
+@REM python main.py --path_dataset_folder %path1% --model %pykeenModel1% --num_epochs 250 --scoring_technique "NegSample" ^
+@REM --batch_size 1024 --lr 0.1 --embedding_dim 256 --trainer "PL" --neg_ratio 1 --pykeen_model_kwargs embedding_dim=64 loss="CrossEntropy" ^
+@REM --use_SLCWALitModule --save_embeddings_as_csv --eval_model "train_val_test" --num_core 1 --accelerator "gpu" --devices 1
+
+@REM python main.py --path_dataset_folder %path1% --model %pykeenModel2% --num_epochs 250 --scoring_technique "NegSample" ^
+@REM --batch_size 1024 --lr 0.1 --embedding_dim 256 --trainer "PL" --neg_ratio 1 --pykeen_model_kwargs embedding_dim=64 loss="CrossEntropy" ^
+@REM --use_SLCWALitModule --save_embeddings_as_csv --eval_model "train_val_test" --num_core 1 --accelerator "gpu" --devices 1
+
+@REM python main.py --path_dataset_folder %path1% --model %diceModel1% --num_epochs 250 --scoring_technique "NegSample" ^
+@REM --batch_size 1024 --lr 0.1 --embedding_dim 256 --trainer "torchDDP" --neg_ratio 1 ^
+@REM --save_embeddings_as_csv --eval_model "train_val_test" --num_core 1 
+
+@REM python main.py --path_dataset_folder %path1% --model %diceModel2% --num_epochs 250 --scoring_technique "NegSample" ^
+@REM --batch_size 1024 --lr 0.1 --embedding_dim 256 --trainer "torchDDP" --neg_ratio 1 ^
+@REM --save_embeddings_as_csv --eval_model "train_val_test" --num_core 1 
